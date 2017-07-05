c79d8a3beaaaf1a04b4bf2ea462fe083029c1a08
burilao77 <mikokoyanosuda772@gmail.com>
2017-07-05 15:56:48 -0400

    creado crud de pedidos y instanceado el dia actual

---
+++ src/Model/Table/OrdersTable.php
@@ -0,0 +1,74 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Orders Model
+ *
+ * @method \App\Model\Entity\Order get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Order newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Order[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Order|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Order patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Order[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Order findOrCreate($search, callable $callback = null, $options = [])
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ */
+class OrdersTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('orders');
+        $this->setDisplayField('name');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->requirePresence('name', 'create')
+            ->notEmpty('name');
+
+        $validator
+            ->decimal('price')
+            ->requirePresence('price', 'create')
+            ->notEmpty('price');
+
+        $validator
+            ->requirePresence('description', 'create')
+            ->notEmpty('description');
+
+/*        $validator
+            ->dateTime('date_expiration')
+            ->requirePresence('date_expiration', 'create')
+            ->notEmpty('date_expiration');*/
+
+        return $validator;
+    }
+}
---
+++ src/Template/Orders/view.ctp
@@ -0,0 +1,48 @@
+<?php
+/**
+  * @var \App\View\AppView $this
+  * @var \App\Model\Entity\Order $order
+  */
+?>
+<nav class="large-3 medium-4 columns" id="actions-sidebar">
+    <ul class="side-nav">
+        <li class="heading"><?= __('Actions') ?></li>
+        <li><?= $this->Html->link(__('Edit Order'), ['action' => 'edit', $order->id]) ?> </li>
+        <li><?= $this->Form->postLink(__('Delete Order'), ['action' => 'delete', $order->id], ['confirm' => __('Are you sure you want to delete # {0}?', $order->id)]) ?> </li>
+        <li><?= $this->Html->link(__('List Orders'), ['action' => 'index']) ?> </li>
+        <li><?= $this->Html->link(__('New Order'), ['action' => 'add']) ?> </li>
+    </ul>
+</nav>
+<div class="orders view large-9 medium-8 columns content">
+    <h3><?= h($order->name) ?></h3>
+    <table class="vertical-table">
+        <tr>
+            <th scope="row"><?= __('Name') ?></th>
+            <td><?= h($order->name) ?></td>
+        </tr>
+        <tr>
+            <th scope="row"><?= __('Id') ?></th>
+            <td><?= $this->Number->format($order->id) ?></td>
+        </tr>
+        <tr>
+            <th scope="row"><?= __('Price') ?></th>
+            <td><?= $this->Number->format($order->price) ?></td>
+        </tr>
+        <tr>
+            <th scope="row"><?= __('Date Expiration') ?></th>
+            <td><?= h($order->date_expiration) ?></td>
+        </tr>
+        <tr>
+            <th scope="row"><?= __('Created') ?></th>
+            <td><?= h($order->created) ?></td>
+        </tr>
+        <tr>
+            <th scope="row"><?= __('Modified') ?></th>
+            <td><?= h($order->modified) ?></td>
+        </tr>
+    </table>
+    <div class="row">
+        <h4><?= __('Description') ?></h4>
+        <?= $this->Text->autoParagraph(h($order->description)); ?>
+    </div>
+</div>
